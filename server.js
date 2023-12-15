efae067a965c6f5ba4ed897d7fe17e0d5b0c774f
Joshua Kisb <joshuakisb@gmail.com>
2023-12-15 09:27:33 +0300

    connect db

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
+
+// connect the database
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log("Database Connected"))
+	.catch((err) => console.error(err));
 
 const port = process.env.PORT || 4243;
 app.listen(port, () => {

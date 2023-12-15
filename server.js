a47ccda838033c18030f8a6efae3637155889146
Joshua Kisb <joshuakisb@gmail.com>
2023-12-15 08:40:36 +0300

    add backend

---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 4243;
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
